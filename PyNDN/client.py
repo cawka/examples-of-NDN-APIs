5b80995a2ab143369914fdfdacfe5db98937f03f
Alexander Afanasyev <alexander.afanasyev@ucla.edu>
2015-04-20 23:31:47 -0700

    PyNDN example

--- PyNDN/client.py
+++ PyNDN/client.py
@@ -1,32 +1,62 @@
 #!/usr/bin/env python
 
-import ndn
+# -*- Mode:python; c-file-style:"gnu"; indent-tabs-mode:nil; -*-
+# Copyright 2013,2015 Alexander Afanasyev
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pyndn as ndn
 import sys
+try:
+    import asyncio
+except ImportError:
+    import trollius as asyncio
+import logging
+logging.basicConfig()
 
-FILENAME = sys.argv[1]
+class Client:
+    def __init__(self, face, filename):
+        self.face = face
+        self.baseName = ndn.Name("/my-local-prefix/simple-fetch/file").append(filename)
+        self.currentSeqNo = 0
 
-face = ndn.Face ()
+        self._requestNext()
 
-InterestBaseName = ndn.Name ("ccnx:/my-local-prefix/simple-fetch/file")
-interest_tmpl = ndn.Interest (scope=1)
+    def _requestNext(self):
+        interest = ndn.Interest(ndn.Name(self.baseName).appendSequenceNumber(self.currentSeqNo))
+        interest.setMustBeFresh(True)
+        self.face.expressInterest(interest, 
+                                  self._onData, self._onTimeout)
+        self.currentSeqNo += 1
 
-def OnData (name, interest, data, kind):
-    print >> sys.stderr, "PyNDN %s" % data.name
-    print data.content,
+    def _onData(self, interest, data):
+        print >> sys.stderr, "PyNDN %s" % data.name
+        print data.content,
 
-    seqnum = ndn.Name.seg2num (data.name[len(data.name)-1])
-    if seqnum >= 10:
-        return ndn.RESULT_OK;
-    
-    face.expressInterestSimple (ndn.Name (InterestBaseName).appendSegment (seqnum+1), 
-                                OnData, OnTimeout, interest_tmpl)
+        seqNum = interest.getName()[-1].toSequenceNumber()
+        if seqNum >= 10:
+            return
+        
+        self._requestNext()
 
-    return ndn.RESULT_OK
+    def _onTimeout(self, interest):
+        self.face.expressInterest(interest.getName(), 
+                                  self._onData, self._onTimeout)
 
-def OnTimeout (name, interest):
-    return ndn.RESULT_REEXPRESS
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    face = ndn.ThreadsafeFace(loop, None)
+    client = Client(face, sys.argv[1])
 
-face.expressInterestSimple (ndn.Name (InterestBaseName).appendSegment (0), 
-                            OnData, OnTimeout, interest_tmpl)
-
-face.run (3000)
+    loop.run_forever()
+    face.shutdown()
--- PyNDN/server.py
+++ PyNDN/server.py
@@ -1,31 +1,66 @@
 #!/usr/bin/env python
 
-import ndn
+# -*- Mode:python; c-file-style:"gnu"; indent-tabs-mode:nil; -*-
+# Copyright 2013,2015 Alexander Afanasyev
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pyndn as ndn
+import pyndn.security as ndnsec
 import sys
+try:
+    import asyncio
+except ImportError:
+    import trollius as asyncio
+import logging
+logging.basicConfig()
 
-face = ndn.Face ()
+class Server:
+    def __init__(self, face):
+        self.face = face
+        self.baseName = ndn.Name("/my-local-prefix/simple-fetch/file")
+        self.counter = 0
+        self.keyChain = ndnsec.KeyChain()
 
-InterestBaseName = ndn.Name ("ccnx:/my-local-prefix/simple-fetch/file")
-COUNTER = 0
+        self.face.setCommandSigningInfo(self.keyChain, self.keyChain.getDefaultCertificateName())
+        self.face.registerPrefix(self.baseName,
+                                 self._onInterest, self._onRegisterFailed)
 
-def OnInterest (name, interest):
-    global COUNTER
-    print >> sys.stderr, "<< PyNDN %s" % interest.name
-            
-    content = "PyNDN LINE #%d\n" % COUNTER
-    COUNTER = COUNTER + 1
-    
-    data = ndn.ContentObject (name = interest.name, content = content, 
-                              signed_info = ndn.SignedInfo (key_digest = ndn.Key.getDefaultKey ().publicKeyID, 
-                                                            freshness = 5,
-                                                            key_locator = ndn.KeyLocator.getDefaultKeyLocator ()))
-    data.sign (ndn.Key.getDefaultKey ())
-    
-    face.put (data)
+    def _onInterest(self, prefix, interest, *k):
+        print >> sys.stderr, "<< PyNDN %s" % interest.name
+                
+        content = "PyNDN LINE #%d\n" % self.counter
+        self.counter += 1
 
-    return ndn.RESULT_OK
+        data = ndn.Data(interest.getName())
 
-face.setInterestFilterSimple (InterestBaseName, OnInterest)
+        meta = ndn.MetaInfo()
+        meta.setFreshnessPeriod(5000)
+        data.setMetaInfo(meta)
 
-while True:
-    face.run (500)
+        data.setContent(content)
+
+        self.keyChain.sign(data, self.keyChain.getDefaultCertificateName())
+        
+        self.face.putData(data)
+
+    def _onRegisterFailed(self, prefix):
+        print >> sys.stderr, "<< PyNDN: failed to register prefix"
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    face = ndn.ThreadsafeFace(loop, None)
+    server = Server(face)
+
+    loop.run_forever()
+    face.shutdown()
